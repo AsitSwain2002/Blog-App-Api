3542d08410b3b1e607777f202b514fcf4c31685c
AsitSwain2002 <147136555+AsitSwain2002@users.noreply.github.com>
2025-03-28 11:17:53 +0530

    Added categoryService Module

--- src/main/java/com/org/Blog_App_Api/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/org/Blog_App_Api/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.org.Blog_App_Api.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,17 +25,32 @@ public class CategoryServiceImpl implements CategoryService {
 
 	@Override
 	public boolean saveCategory(CategoryDto categoryDto) {
-		Category category = mapper.map(categoryDto, Category.class);
 
+		// validation
+
+		Category category = mapper.map(categoryDto, Category.class);
 		// Name Already Present or not Check
 		categoryAlreadyPrsent(category.getName());
+		// update logic
+		if (category.getId() != null) {
+			updateCategory(category);
+		}
 		Category save = categoryRepo.save(category);
 		if (ObjectUtils.isEmpty(save)) {
 			return false;
 		}
 		return true;
 	}
-     
+
+	// update Category method
+	private void updateCategory(Category categoryIn) {
+
+		int catId = categoryIn.getId();
+		Category category = categoryRepo.findById(catId)
+				.orElseThrow(() -> new ResourceNotFoundException("Category with id '" + catId + "' Not Found"));
+		category.setName(categoryIn.getName());
+	}
+
 	private void categoryAlreadyPrsent(String name) {
 		boolean existsByNameAndIsDeletedFalse = categoryRepo.existsByNameAndIsDeletedFalse(name);
 		if (existsByNameAndIsDeletedFalse) {
@@ -48,6 +64,18 @@ public class CategoryServiceImpl implements CategoryService {
 				.orElseThrow(() -> new ResourceNotFoundException("Category with id '" + id + "' Not Found"));
 
 		return mapper.map(category, CategoryDto.class);
+	}
+
+	@Override
+	public List<CategoryDto> findAllCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCategory(Integer id) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
